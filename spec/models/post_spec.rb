f061a16a96d59a55ae9d9ffd224a5a9a61553dec
Neetu tiwari <neetutiwarihld@gmail.com>
2023-02-16 11:11:42 -0800

    My first git commit

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'Post' do           #
+    context 'before publication' do  # (almost) plain English
+      it 'cannot have comments' do   #
+        expect { Post.create.comments.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
+      end
+    end
+  end
